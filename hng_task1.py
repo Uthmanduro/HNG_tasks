bb5070d861b66736feb04bd029395b0995980077
uthmanduro <udurosinlohun@gmail.com>
2023-09-07 22:26:42 +0100

    fixed utc field

--- hng_task1.py
+++ hng_task1.py
@@ -44,7 +44,7 @@ def index():
     response = {
         "slack_name": slack_name,
         "current_day": current_day,
-        "current_time": current_time,
+        "utc_time": current_time,
         "track": track,
         "github_file_url": github_file_url,
         "github_repo_url": github_repo_url,
